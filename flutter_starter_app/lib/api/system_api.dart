ad20bf6e43a2929f616ec82c10536dc45db51c9a
zeynepdurmus <zeynep.durmus@spike.com.tr>
2023-05-27 23:34:46 +0300

    edited

--- flutter_starter_app/lib/api/system_api.dart
+++ flutter_starter_app/lib/api/system_api.dart
@@ -32,6 +32,32 @@ class SystemApi {
     }
   }
 
+  me({required String token}) async {
+    try {
+      String apiEndpoint = "$AppLink/me";
+
+      final Dio dio = Dio();
+      dio.options.headers["authorization"] = "Bearer $token";
+
+      var response = await dio.get(apiEndpoint);
+      if (response.statusCode == 200) {
+        if (response.data["success"]) {
+          var res = response.data;
+          return res;
+        } else {
+          return null;
+        }
+      } else if (response.statusCode == 404) {
+        // wrong user password
+        return response.data;
+      } else {
+        return null;
+      }
+    } catch (e) {
+      return null;
+    }
+  }
+
   Future<String?> register(
     {required String email, 
     required  String password, 
